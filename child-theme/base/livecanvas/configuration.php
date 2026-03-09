549d8ba0fe364c9172d1dbbdae034d5d9734565a
supercharles76 <74455199+supercharles76@users.noreply.github.com>
2026-03-09 15:37:49 +0100

    Added pre-made sections and pages to child base

---
+++ child-theme/base/livecanvas/configuration.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// DEFINE A CONFIGURATION FOR THE LIVECANVAS EDITOR
+function lc_define_editor_config($key) {
+    $data =  [
+        'config_file_slug' => 'daisyui-5.js', 
+    ];
+    return $data[$key];
+}
+
